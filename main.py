d9b052360613b9cb1fb6eb35dc9a0bdf08d9531c
denrele15 <78202883+denrele15@users.noreply.github.com>
2021-01-30 11:51:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import string
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+#removing stopwords and noise
+text = open('read.txt', encoding='utf-8').read()
+lower_case = text.lower()  # in lower case
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))  # remove all punctuation marks in text
+tokenized_words = cleaned_text.split()  # tokenized words(breaking the words)
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+clean_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        clean_words.append(word)
+print(clean_words)
+
+#NLP Emotion Algorithm
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace('\n', '').replace(',', '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+        #print ("word:" + word + "; " + "Emotion:" + emotion)
+        if word in clean_words:
+            emotion_list.append(emotion)
+
+            print (emotion_list)
+            w = Counter(emotion_list)
+            print(w )
+
+
+#Plotting the emotion graph
+    fig, ax1 = plt.subplots()
+    ax1.bar(w.keys(), w.values())
+    fig.autofmt_xdate()
+    plt.savefig('graph.png')
+    plt.show()
